199924e4e1650c451bd96a50904dcb57bb817105
H-COOOH <h-cooh@foxmail.com>
2021-02-05 13:20:22 +0800

    new

---
+++ an.js
@@ -0,0 +1,19 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c17d7b0132e934ae897cfd88e64eb301";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://www.googletagmanager.com/gtag/js?id=G-3TG9FTM46F";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-3TG9FTM46F');
